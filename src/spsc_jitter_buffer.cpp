09181785a5dd38743d57aea0377efd3dca3e1518
Marc <marc.weber54@gmail.com>
2023-09-21 19:38:05 -0600

    Attempt to lower latency by clearing the janky queue

--- src/spsc_jitter_buffer.cpp
+++ src/spsc_jitter_buffer.cpp
@@ -13,11 +13,18 @@ SPSCJitterBuffer::~SPSCJitterBuffer(){
 void SPSCJitterBuffer::push_samples(int timestamp, godot::PackedVector2Array samples){
     int num_written = sample_queue.write(samples.size(), samples.ptr());
 
+    if(num_written < samples.size()){
+        godot::UtilityFunctions::print("TOO MANY SAMPLES - samples: ", samples.size(), " written: ", num_written);
+        godot::PackedVector2Array temp;
+        temp.resize(2048);
+        sample_queue.read(temp.size(), (godot::Vector2*) temp.ptr()); // Skip forward
+    }
     // godot::UtilityFunctions::print("num_written: ",num_written);
 }
 
 void SPSCJitterBuffer::pop_samples(godot::AudioFrame* samples, int frames){
     int num_read = sample_queue.read(frames, (godot::Vector2*) samples);
 
+    if(num_read < frames) godot::UtilityFunctions::print("NOT ENOUGH SAMPLES - frames: ", frames, " num_read: ", num_read);
     // godot::UtilityFunctions::print("num_read: ",num_read);
 }
